4775bcd6e781eb9dde0800d0d50d16467ae54b73
Thordur Hermannsson <doddi@kott.is>
2019-11-25 00:47:16 +0000

    adding getHTMLString method

---
+++ src/Quill/API/HTML.js
@@ -0,0 +1,5 @@
+'use strict';
+
+exports.getHTMLStringImpl = function(self) {
+    return self.root.innerHTML
+}
---
+++ src/Quill/API/HTML.purs
@@ -0,0 +1,24 @@
+module Quill.API.HTML 
+  ( getHTMLString )
+where
+
+import Prelude
+import Control.Monad.Error.Class    (class MonadError
+                                    ,throwError)
+import Control.Monad.Except         (runExcept)
+import Data.Either                  (Either(..), either)
+import Effect                       (Effect)
+import Effect.Class                 (class MonadEffect
+                                    ,liftEffect)
+import Effect.Uncurried             as UncurriedEffect
+import Foreign                      as Foreign
+import Quill.Editor                 (Editor)
+
+foreign import getHTMLStringImpl :: UncurriedEffect.EffectFn1 Editor Foreign.Foreign
+
+getHTMLString :: forall m
+               . MonadEffect m 
+              => MonadError Foreign.MultipleErrors m
+              => Editor 
+              -> m String
+getHTMLString editor = either throwError pure <<< runExcept <<< Foreign.readString <=< liftEffect $ UncurriedEffect.runEffectFn1 getHTMLStringImpl editor
