80da7ccd49c688530d6e7853179a56c50cdfc42a
Thordur Hermannsson <doddi@kott.is>
2019-12-18 01:00:23 +0000

    jam stuff into config until we sort out implementation

--- src/Quill/Editor.js
+++ src/Quill/Editor.js
@@ -1,5 +1,10 @@
 'use strict';
 
 exports.newImpl = function(el, config) {
+    var modules = {
+        syntax: true,
+        toolbar: [config.formats]
+    }
+    config.modules = modules;
     return new Quill(el, config);
 };
